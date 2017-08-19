f27676f7974bdda4f64afec80c4ef627f61ec85b
Anastasiya Vasina <nast791@mail.ru>
2017-08-19 17:45:22 +0300

    Все, кроме Трэвиса

--- js/pictures.js
+++ js/pictures.js
@@ -1,70 +1,90 @@
 'use strict';
 
-var comments = [
-  'Всё отлично!', 'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках, и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота, и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-];
+(function () {
+  function getRandomComment() {
+    var comments = [
+      'Всё отлично!', 'В целом всё неплохо. Но не всё.',
+      'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+      'Моя бабушка случайно чихнула с фотоаппаратом в руках, и у неё получилась фотография лучше.',
+      'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота, и у меня получилась фотография лучше.',
+      'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+    ];
 
-function generateNumber(min, max) {
-  var rand = min + Math.random() * (max + 1 - min);
-  rand = Math.floor(rand);
-  return rand;
-}
+    var commentIndex = generateNumber(0, comments.length - 1);
+    return comments[commentIndex];
+  }
 
-var photoInfo = {
-  url: 'photos/{{' + (i + 1) + '}}.jpg',
-  likes: generateNumber(15, 200),
-  comments: comments[generateNumber(0, comments.length)]
-};
+  function generateNumber(min, max) {
+    var rand = min + Math.random() * (max + 1 - min);
+    rand = Math.floor(rand);
+    return rand;
+  }
 
-alert(photoInfo.url);
+  function getPhotoUrl() {
+    var url = [];
+    for (var i = 1; i <= 25; i++) {
+      url.push('photos/' + i + '.jpg');
+    }
+    return url;
+  }
 
-var data = [];
+  var data = getPhotoUrl();
 
-for (var i = 0; i < 25; i++) {
-  photoInfo.url = 'photos/{{' + (i + 1) + '}}.jpg';
-  data.push(photoInfo);
-}
+  function generatePhotoArray() {
+    var photo;
 
-alert(data[2].url);
+    var photoArray = data.map(function (index) {
+      photo = {
+        url: index,
+        likes: generateNumber(15, 200),
+        comments: getRandomComment()
+      };
+      return photo;
+    });
+    return photoArray;
+  }
 
-// Галлерея на сайте
-var pictureTemplate = document.querySelector('#picture-template');
-var picturesContainer = document.querySelector('.pictures');
-var uploadOverlay = document.querySelector('.upload-overlay');
-uploadOverlay.classList.add('hidden');
-var gallery = document.querySelector('.gallery-overlay');
-gallery.classList.remove('hidden');
+  // Галлерея на сайте
+  var photosArray = generatePhotoArray(data);
+  var picturesContainer = document.querySelector('.pictures');
 
-function getPicture(data) {
-  var picture = pictureTemplate.cloneNode(true);
+  var uploadOverlay = document.querySelector('.upload-overlay');
+  uploadOverlay.classList.add('hidden');
 
-  picture.querySelector('img').src = data.url;
-  picture.querySelector('.picture-likes').textContent = data.likes;
-  picture.querySelector('.picture-comments').textContent = data.comments;
+  var gallery = document.querySelector('.gallery-overlay');
+  gallery.classList.remove('hidden');
 
-  return picture;
-}
+  function setPicture(photosArray) {
+    var pictureTemplate = document.querySelector('#picture-template').content;
+    var picture = pictureTemplate.cloneNode(true);
 
-var fragment = document.createDocumentFragment();
-for (var i = 0; i < data.length; i++) {
-  fragment.appendChild(getPicture(data[i]));
-}
-picturesContainer.appendChild(fragment);
+    picture.querySelector('img').src = photosArray.url;
+    picture.querySelector('.picture-likes').textContent = photosArray.likes;
+    picture.querySelector('.picture-comments').textContent = photosArray.comments.length;
 
-function getPictureInGallery(data) {
-  var galleryPreview = gallery.querySelector('.gallery-overlay-preview');
+    return picture;
+  }
 
-  galleryPreview.querySelector('.gallery-overlay-image').src = data.url;
-  galleryPreview.querySelector('.likes-count').textContent = data.likes;
-  galleryPreview.querySelector('.comments-count').textContent = data.comments;
+  function appendFragments() {
+    var fragment = document.createDocumentFragment();
 
-  return galleryPreview;
-}
+    for (var i = 0; i < photosArray.length; i++) {
+      fragment.appendChild(setPicture(photosArray[i]));
+    }
+    return fragment;
+  }
 
-for (var i = 0; i < data.length; i++) {
-  gallery.appendChild(getPictureInGallery(data[i]));
-}
+  picturesContainer.appendChild(appendFragments(photosArray));
+
+  function getPictureInGallery(photosArray) {
+    var galleryPreview = gallery.querySelector('.gallery-overlay-preview');
+
+    galleryPreview.querySelector('.gallery-overlay-image').src = photosArray.url;
+    galleryPreview.querySelector('.likes-count').textContent = photosArray.likes;
+    galleryPreview.querySelector('.comments-count').textContent = photosArray.comments.length;
+
+    return galleryPreview;
+  }
+
+  gallery.appendChild(getPictureInGallery(photosArray[13]));
+})();

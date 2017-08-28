7c14cb912bde47664637bd62c41a78b47ae904d8
Anastasiya Vasina <nast791@mail.ru>
2017-08-28 03:14:39 +0300

    Обновленная версия

--- js/pictures.js
+++ js/pictures.js
@@ -156,7 +156,7 @@
   // Форма
   var form = document.querySelector('.upload-form');
   var uploadFile = form.querySelector('.upload-input');
-  var uploadOverlay = form.querySelector('.upload-overlay');
+  var formUploadOverlay = form.querySelector('.upload-overlay');
   var cancelBtn = form.querySelector('.upload-form-cancel');
   var descriptionForm = form.querySelector('.upload-form-description');
   var submitBtn = form.querySelector('.upload-form-submit');
@@ -164,6 +164,8 @@
   var formResizeMinus = form.querySelector('.upload-resize-controls-button-dec');
   var formResizePlus = form.querySelector('.upload-resize-controls-button-inc');
   var scaleImage = form.querySelector('.effect-image-preview');
+  // var hashtags = form.querySelector('.upload-form-hashtags');
+  var effectControls = form.querySelector('.upload-effect-controls');
 
   function pressEscForm(evt) {
     if (evt.keyCode === ESC_KEYCODE) {
@@ -172,12 +174,12 @@
   }
 
   function openForm() {
-    uploadOverlay.classList.remove('hidden');
+    formUploadOverlay.classList.remove('hidden');
     document.addEventListener('keydown', pressEscForm);
   }
 
   function closeForm() {
-    uploadOverlay.classList.add('hidden');
+    formUploadOverlay.classList.add('hidden');
     document.removeEventListener('keydown', pressEscForm);
   }
 
@@ -204,27 +206,58 @@
     }
   });
 
-  // !!Все равно отправляет форму, даже если есть ошибки, как исправить?
-  submitBtn.addEventListener('click', function (evt) {
-    form.submit();
-  });
-
-  descriptionForm.addEventListener('input', function (evt) {
-    var target = evt.target;
-    if (target.value.length < 30) {
-      target.setCustomValidity('Комментарий должен состоять минимум из 30 символов');
+  function validateDescription() {
+    if (descriptionForm.value.length < 30) {
+      descriptionForm.setCustomValidity('Комментарий должен состоять минимум из 30 символов');
+      descriptionForm.setAttribute('style', 'box-shadow: 0 0 0 1px red;');
+      return false;
     } else
-    if (target.value.length > 100) {
-      target.setCustomValidity('Комментарий должен состоять максимум из 100 символов');
+    if (descriptionForm.value.length > 100) {
+      descriptionForm.setCustomValidity('Комментарий должен состоять максимум из 100 символов');
+      descriptionForm.setAttribute('style', 'box-shadow: 0 0 0 1px red;');
+      return false;
     } else {
-      target.setCustomValidity('');
+      descriptionForm.setCustomValidity('');
+      return true;
+    }
+  }
+
+  // !! Не знаю, как сделать валидацию хеш-тегов, ничего не работает
+  // function validateHashtags() {
+    // hashtags.addEventListener('focus', function (evt) {
+      // var hashtagsArr = hashtags.value.split(' ');
+      // console.log(hashtagsArr);
+      // var filterHashtags = /^\#[0-9a-z]+/i;
+
+      // for (i = 0; i < hashtagsArr.length; i++) {
+      // if ((hashtagsArr.length <= 5) && (hashtagsArr.length >= 0)) {
+        // return false;
+      // }
+      // else
+      // if (filterHashtags.test(hashtagsArr[i])) {
+      //   return true;
+      // }
+
+      // else {
+      //   return true;
+      // }
+    // });
+    // }
+  // }
+
+  submitBtn.addEventListener('click', function (evt) {
+    if (validateDescription()) {
+      return true;
+    } else {
+      return false;
     }
   });
 
-  // !!Все равно отправляет форму, даже если есть ошибки, как исправить?
   submitBtn.addEventListener('keydown', function (evt) {
-    if ((evt.keyCode === ENTER_KEYCODE) && (descriptionForm.validity.valid)) {
-      form.submit();
+    if ((evt.keyCode === ENTER_KEYCODE) && (validateDescription())) {
+      return true;
+    } else {
+      return false;
     }
   });
 
@@ -242,24 +275,40 @@
   formResizeControls.value = '100%';
 
   formResizeMinus.addEventListener('click', function (evt) {
-    var formResizeCtrls = Number(formResizeControls.value.replace(/%/g,''));
+    var formResizeCtrls = Number(formResizeControls.value.replace(/%/g, ''));
     formResizeCtrls -= 25;
     formResizeControls.value = formResizeCtrls + '%';
-    var transform = 'scale(0.' + (formResizeControls.value.replace(/%/g,'')) + ')';
+    var transform = 'scale(0.' + (formResizeControls.value.replace(/%/g, '')) + ')';
     scaleImage.setAttribute('style', ('transform: ' + transform));
 
-    getMinMax (formResizeCtrls);
+    getMinMax(formResizeCtrls);
   });
 
   formResizePlus.addEventListener('click', function (evt) {
-    var formResizeCtrls = Number(formResizeControls.value.replace(/%/g,''));
+    var formResizeCtrls = Number(formResizeControls.value.replace(/%/g, ''));
     formResizeCtrls += 25;
     formResizeControls.value = formResizeCtrls + '%';
-    var transform = 'scale(0.' + (formResizeControls.value.replace(/%/g,'')) + ')';
+    var transform = 'scale(0.' + (formResizeControls.value.replace(/%/g, '')) + ')';
     scaleImage.setAttribute('style', ('transform: ' + transform));
 
-    getMinMax (formResizeCtrls);
+    getMinMax(formResizeCtrls);
   });
 
+  function onEffectControlsClick() {
+    effectControls.addEventListener('click', function (evt) {
+      evt.preventDefault();
+      var target = evt.target;
+      var parentTarget = target.parentNode;
+      var attributeFor = parentTarget.getAttribute('for');
+      var key = attributeFor.replace(/upload-/, '');
+      scaleImage.classList.add(key);
+      var classes = scaleImage.className.split(' ');
 
+      if (classes.length > 2) {
+        scaleImage.classList.remove(classes[1]);
+        classes.splice(1, 1);
+      }
+    });
+  }
+  onEffectControlsClick();
 })();
